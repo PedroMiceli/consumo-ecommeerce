e2305912168843561817e2a1a943230881397982
PedroMiceli <pedro.miceli45@gmail.com>
2026-06-03 18:57:36 -0300

    remap do repo para ter a ui tambem

--- src/main/java/com/consumo_ecommerce/consumo_ecommerce/exceptions/NaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.consumo_ecommerce.consumo_ecommerce.exceptions;
-
-public class NaoEncontradoException extends RuntimeException{
-    public NaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-}
--- src/main/java/com/consumo_ecommerce/consumo_ecommerce/model/repositories/VendaImportacaoErroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.consumo_ecommerce.consumo_ecommerce.model.repositories;
-
-import com.consumo_ecommerce.consumo_ecommerce.model.models.venda.VendaImportacaoErro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface VendaImportacaoErroRepository extends JpaRepository<VendaImportacaoErro, UUID> {
-}
